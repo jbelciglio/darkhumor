640cda39ef8a279ff27f959a2d56c6a7ab9dfa12
pauline-ann <paulinebantayan@gmail.com>
2019-08-01 23:15:38 -0700

    404 error component + route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header.js';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contacts from './components/Contacts';
 import Culture from './components/Culture';
 import Future from'./components/Future';
+import Error404 from './components/Error404'
 
 import './App.css';
 
@@ -15,13 +16,16 @@ class App extends React.Component {
   render(){
   return (
     <Router>
-    <div className="Container">
-    <Header />
-    <Route exact path="/" component={()=> <Home />} />
-    <Route exact path="/projects" component={()=> <Projects />} />
-    <Route exact path="/contacts" component={()=> <Contacts />} />
-    <Route exact path="/culture" component={()=> <Culture />} />
-    <Route exact path="/future" component={()=> <Future />} />
+    <div>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={()=> <Home />} />
+          <Route exact path="/projects" component={()=> <Projects />} />
+          <Route exact path="/contacts" component={()=> <Contacts />} />
+          <Route exact path="/culture" component={()=> <Culture />} />
+          <Route exact path="/future" component={()=> <Future />} />
+          <Route component={Error404}/>
+        </Switch>
     </div>
     </Router>
   );
