cd7aa572a4158f08600f3954f940fcd002409146
jbelciglio <jbelciglio@gmail.com>
2019-08-08 17:14:08 -0700

    visual changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "./Header.css";
 import axios from "axios";
+import darkHumor from '../assets/images/darkHumor.png'
 
 class Header extends React.Component {
     componentDidMount() {
@@ -11,7 +12,7 @@ class Header extends React.Component {
         return (
             <div class="pos-f-t">
   <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
+    <div class="p-4" id="navInt">
     <Link to="/Projects" id="navLink">Projects</Link>
     <Link to="/Contacts" id="navLink">Contacts</Link>
     <Link to="/Culture" id="navLink">Our Culture</Link>
@@ -28,15 +29,20 @@ class Header extends React.Component {
       
     </div>
   </div>
-  <nav class="navbar navbar-dark bg-dark">
+  
+  <nav class="navbar navbar-light" id="navMain">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div id="logoText">
     
-    <Link to="/" h5 class="text-white h4">Dark Humor Studios</Link>
+
+    <div  id="logoText">
+    <img src={darkHumor} width="35" height="35" alt="logo" id="logoNav" />
+    <Link to="/" h5 class="text h4" id="navTitle">Dark Humor Studios</Link>
+    
     </div>
   </nav>
+  
 </div>
         )
             
